1cf5b199e9a004e9ab437de5814f4319acf6c64f
Dipankar Barman <barman498@gmail.com>
2021-07-29 17:49:48 +0530

    git file added

--- git.js
+++ git.js
@@ -1,3 +1,10 @@
 // Git Commands
 
 // 1. git init --> initialise a git repository
+// 2. git status ---> it tracks all your files
+// 3. git add <filename> --- add your files to local
+// 4. git add . ----> add all files together
+// 5. git commit -m "write your commit message"
+// 6. git push
+
+// git, github/GitLab/AWS CodeCommit
